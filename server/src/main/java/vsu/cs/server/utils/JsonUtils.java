06f90d254e7a8eed0b1cb03fc1d5bc819ee5a1b4
Mishka <Fiz-mat228@yandex.ru>
2023-04-30 17:57:09 +0300

    added type object

--- server/src/main/java/vsu/cs/server/utils/JsonUtils.java
+++ server/src/main/java/vsu/cs/server/utils/JsonUtils.java
@@ -14,36 +14,8 @@ public class JsonUtils {
         ObjectMapper objectMapper = new ObjectMapper();
         StringBuilder stringBuilder = new StringBuilder();
 
-        int level = 1;
-
         stringBuilder.append("{\n");
-        stringBuilder.append("\t".repeat(level));
-        stringBuilder.append("\"title\": \"Result form\",\n");
-
-        stringBuilder.append("\t".repeat(level));
-        stringBuilder.append("\"type\": \"object\",\n");
-
-        stringBuilder.append("\t".repeat(level));
-        stringBuilder.append("\"properties\": {\n");
-        level++;
-
-        Iterator<Entry<String, JsonNode>> iter = json.fields();
-
-        while (iter.hasNext()) {
-            Entry<String, JsonNode> entryJson = iter.next();
-            if (entryJson.getValue().isValueNode()) {
-                appendSimpleData(stringBuilder, entryJson.getKey(), entryJson.getValue(), level);
-            }
-            if (iter.hasNext()) {
-                stringBuilder.append("},\n");
-            } else {
-                stringBuilder.append("}\n");
-            }
-        }
-
-        level--;
-        stringBuilder.append("\t".repeat(level));
-        stringBuilder.append("}\n");
+        processTyping(stringBuilder, json, "Result Form", "object", 1);
         stringBuilder.append("}");
 
         try {
@@ -54,6 +26,50 @@ public class JsonUtils {
             log.error("Error JSON: " + ex.getMessage());
             return null;
         }
+    }
+
+    private static void processTyping(StringBuilder stringBuilder, JsonNode json, String name,
+                                      String type, int level) {
+        stringBuilder.append("\t".repeat(level));
+        stringBuilder.append("\"title\": \"")
+                .append(name)
+                .append("\",\n");
+
+        stringBuilder.append("\t".repeat(level));
+        stringBuilder.append("\"type\": \"")
+                .append(type)
+                .append("\",\n");
+
+        stringBuilder.append("\t".repeat(level));
+        stringBuilder.append("\"properties\": {\n");
+        Iterator<Entry<String, JsonNode>> iter = json.fields();
+
+        while (iter.hasNext()) {
+            Entry<String, JsonNode> entryJson = iter.next();
+            if (entryJson.getValue().isValueNode()) {
+                appendSimpleData(stringBuilder, entryJson.getKey(), entryJson.getValue(), level + 1);
+            } else if (entryJson.getValue().isObject()) {
+
+                stringBuilder.append("\t".repeat(level + 1));
+                stringBuilder.append("\"")
+                        .append(entryJson.getKey())
+                        .append("\"")
+                        .append(": {\n");
+
+                processTyping(stringBuilder, entryJson.getValue(), entryJson.getKey(),
+                        entryJson.getValue().getNodeType().toString().toLowerCase(), level + 2);
+
+                stringBuilder.append("\t".repeat(level + 1));
+            }
+            if (iter.hasNext()) {
+                stringBuilder.append("},\n");
+            } else {
+                stringBuilder.append("}\n");
+            }
+        }
+
+        stringBuilder.append("\t".repeat(level));
+        stringBuilder.append("}\n");
     }
 
     private static void appendSimpleData(StringBuilder stringBuilder, String field, JsonNode value, int level) {
